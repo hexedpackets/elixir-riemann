268fa00a7455251ed6ce9ebe6f499e79f0b05a53
William Huba <hexedpackets@gmail.com>
2017-02-17 16:59:55 -0500

    Naked elixir project

---
+++ lib/riemann.ex
@@ -0,0 +1,18 @@
+defmodule Riemann do
+  @moduledoc """
+  Documentation for Riemann.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Riemann.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/riemann_test.exs
@@ -0,0 +1,8 @@
+defmodule RiemannTest do
+  use ExUnit.Case
+  doctest Riemann
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
