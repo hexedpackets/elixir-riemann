12babb53a76eacab1fdb44d6ebc452907d815b78
William Huba <hexedpackets@gmail.com>
2017-02-22 15:37:55 -0500

    Process single events in addition to lists of events.

--- lib/riemann.ex
+++ lib/riemann.ex
@@ -2,23 +2,22 @@ defmodule Riemann do
   @doc """
   Send a list of events synchronously.
   """
-  @spec submit(nonempty_list) :: :ok | {:error, atom}
-  def submit(events) do
+  def submit(events) when is_list(events) do
     events
     |> events_to_msg
     |> Riemann.Client.submit
   end
+  def submit(event), do: [event] |> submit
 
   @doc """
   Send a list of events asynchronously.
   """
-  @spec submit_async(nonempty_list) :: :ok | {:error, atom}
-  def submit_async(events) do
+  def submit_async(events) when is_list(events) do
     events
     |> events_to_msg
     |> Riemann.Client.submit_async
   end
-
+  def submit_async(event), do: [event] |> submit_async
 
   defp events_to_msg(events) do
     [events: Enum.map(events, &Riemann.Event.build/1)]
